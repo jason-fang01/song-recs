ca146bee7a64102e0a8b3ed4b659db4bbc44cdda
Jason Fang <n10755888@qut.edu.au>
2023-09-17 21:57:00 +1000

    Fixed deprecated issue warning.

--- spotifyService.js
+++ spotifyService.js
@@ -10,7 +10,7 @@ async function authenticateWithSpotify() {
 			headers: {
 				Authorization:
 					"Basic " +
-					new Buffer(
+					Buffer.from(
 						`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
 					).toString("base64"),
 				"Content-Type": "application/x-www-form-urlencoded",
